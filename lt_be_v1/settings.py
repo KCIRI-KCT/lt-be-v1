b58237009fc62a20edb986e9bafa617cf25ed6aa
kartheeswaranAR <kartheeswaran.dev@outlook.com>
2026-09-05 18:26:55 +0530

    RK-ltbev1-Allowed the regions

--- lt_be_v1/settings.py
+++ lt_be_v1/settings.py
@@ -339,7 +339,7 @@ else:
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
     ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
     'accept',
